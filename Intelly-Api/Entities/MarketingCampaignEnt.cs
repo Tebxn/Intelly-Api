89d80d55d01a24f86fa6f2cd85743546940e85ed
Esteban Sanchez <easanrui@gmail.com>
2023-11-02 19:17:13 -0600

    Creating entities ProductEnt and MarketingCampaignEnt

---
+++ Intelly-Api/Entities/MarketingCampaignEnt.cs
@@ -0,0 +1,12 @@
+﻿namespace Intelly_Api.Entities
+{
+    public class MarketingCampaignEnt
+    {
+        public long MarketingCampaign_Id { get; set; }
+        public long MarketingCampaign_CompanyId { get; set; }
+        public string? MarketingCampaign_Internal_Code { get; set; } = string.Empty;
+        public string? MarketingCampaign_Name { get; set; } = string.Empty;
+        public DateTime? MarketingCampaign_Start_Date { get; set; }
+        public DateTime? MarketingCampaign_End_Date { get; set; }
+    }
+}
